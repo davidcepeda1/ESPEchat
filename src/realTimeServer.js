b19544ae01950713ec2ed1ffeb497bf65ea8bf2f
davidcepeda1 <davidcepeda751@gmail.com>
2026-05-06 10:45:05 -0500

    feat: Añadido estilos para index.html y la creacion del registro

--- src/realTimeServer.js
+++ src/realTimeServer.js
@@ -2,6 +2,6 @@ module.exports = (httpServer) => {
   const { Server, Socket } = require("socket.io");
   const io = new Server(httpServer);
   io.on("connection", (socket) => {
-    console.log("a user connected");
+    console.log(socket.id, "a user connected");
   });
 };
