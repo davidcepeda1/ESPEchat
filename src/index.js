d75abd8e5eacc004f1103bd0d08623d521e10863
davidcepeda1 <davidcepeda751@gmail.com>
2026-05-06 10:09:43 -0500

    feat: Creacion de la ruta para obtener la vista index.html, script para inicio de aplicacion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const express = required("express");
+const express = require("express");
 const { createServer } = require("http");
 const path = require("path");
+const realTimeServer = require("./realTimeServer");
 
 const app = express();
 const httpServer = createServer(app);
@@ -15,3 +16,5 @@ app.use(express.static(path.join(__dirname, "public")));
 httpServer.listen(app.get("port"), () => {
   console.log(`Server running on port ${app.get("port")}`);
 });
+
+realTimeServer(httpServer);
